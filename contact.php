37ab83d53fcd72caea407f4827d4ad507d9c5986
Anmar Abdi <anmarabdi@yahoo.com>
2022-04-22 10:27:05 -0500

    steven added review form

--- contact.php
+++ contact.php
@@ -1,51 +1,41 @@
 <?php
   include 'header.php';
   ?>
+<head>
+  <link rel="stylesheet" href="review.css">
+</head>
 <br>
 <br>
 <div class="container-fluid padding">
     <div class="row welcome text-center">
         <div class="col-12">
-            <h1 class="display-4 about-h1">Customer Feedback</h1>
+            <h1 class="display-4 about-h1">Customer Feedback:</h1>
         </div>
         <br>
     </div>
 </div>
-<div class="container">
-    <p class="words col-sm">Where We're Located</p> 
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14092.755158991886!2d86.91622033000927!3d27.988119872821486!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39e854a215bd9ebd%3A0x576dcf806abbab2!2sMt%20Everest!5e0!3m2!1sen!2sus!4v1650467974229!5m2!1sen!2sus" class="google-maps"></iframe>
+<div class="reviews">
+    <script>
+    const reviews_page_id = 1;
+    fetch("reviews.php?page_id=" + reviews_page_id).then(response => response.text()).then(data => {
+        document.querySelector(".reviews").innerHTML = data;
+        document.querySelector(".reviews .write_review_btn").onclick = event => {
+            event.preventDefault();
+            document.querySelector(".reviews .write_review").style.display = 'block';
+            document.querySelector(".reviews .write_review input[name='name']").focus();
+        };
+        document.querySelector(".reviews .write_review form").onsubmit = event => {
+            event.preventDefault();
+            fetch("reviews.php?page_id=" + reviews_page_id, {
+                method: 'POST',
+                body: new FormData(document.querySelector(".reviews .write_review form"))
+            }).then(response => response.text()).then(data => {
+                document.querySelector(".reviews .write_review").innerHTML = data;
+            });
+        };
+    });
+    </script>
 </div>
-
-<?php
-
-
- 
- 
- if (isset($_GET["signup"])) {
-  if ($_GET["signup"] == "success") {
-  echo '<p>Signup successful!</p>'; 
- }
-}
-?>
-<div class="container d-flex justify-content-center">
-  <div class="row">
-    <div class="card">
-    <p class="words col-sm">Give Us Feedback!</p> 
-      <form action="contact.php" method="post">
-        <div class="form-group">
-          <input type="text" name="uid" placeholder="Name">
-          <input type="text" name="mail" placeholder="Email">
-          <input type="text" name="msg" placeholder="Message">
-        </div>
-        <button type="submit" name="signup-submit" class="btn btn-primary">Submit</button>
-      </form>
-    </div>
-  </div>
-</div>
-</main>
-
-
-
 
 
 
--- shop.php
+++ shop.php
@@ -33,7 +33,7 @@
 						</ul>
 					</div>
 				</div>
-				<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+				<p class="card-text"><small class="text-muted">Last updated 30 mins ago</small></p>
 			</div>
 
 			<!--Start of Modal Testing-->
@@ -77,14 +77,25 @@
 	<div class="row no-gutters">
 		<div class="col-md-8">
 			<div class="card-body">
-				<h5 class="card-title">Card title</h5>
+				<h5 class="card-title">CPU</h5>
 				<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
 					content. This content is a little bit longer.</p>
-				<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+					<div class="col-5">
+						<p>Specs</p>
+						<ul>
+							<li>
+								<p class="specslist">Faster loading times!</p>
+							</li>
+							<li>
+								<p class="specslist">Get it now!</p>
+							</li>
+						</ul>
+					</div>
+				<p class="card-text"><small class="text-muted">Last updated 5 mins ago</small></p>
 			</div>
 		</div>
 		<div class="col-md-4">
-			<img src="./img/ram.PNG" class="card-img" alt="Ram Stick">
+			<img src="https://drive.google.com/uc?export=view&id=1wipESP65C7hSdSSwC3GirUbTo_vnqpKs" class="card-img" alt="CPU">
 		</div>
 	</div>
 </div>
@@ -93,14 +104,24 @@
 <div class="card mb-3" style="max-width: 75%;">
 	<div class="row no-gutters">
 		<div class="col-md-4">
-			<img src="./img/ram.PNG" class="card-img" alt="Ram Stick">
+			<img src="https://drive.google.com/uc?export=view&id=1J4pURheuUYpMpWZDPwzsoBGZg4Gngixn" class="card-img" alt="GPU">
 		</div>
 		<div class="col-md-8">
 			<div class="card-body">
-				<h5 class="card-title">Card title</h5>
-				<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
-					content. This content is a little bit longer.</p>
-				<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+				<h5 class="card-title">GPU</h5>
+				<p class="card-text">The GPU is a specialized processor designed to accelerate graphics rendering and accelerate the creation of images.</p>
+				<div class="col-5">
+						<p>Specs</p>
+						<ul>
+							<li>
+								<p class="specslist">Faster loading times!</p>
+							</li>
+							<li>
+								<p class="specslist">Get it now!</p>
+							</li>
+						</ul>
+					</div>
+				<p class="card-text"><small class="text-muted">Last updated 56 mins ago</small></p>
 			</div>
 		</div>
 	</div>
@@ -111,14 +132,25 @@
 	<div class="row no-gutters">
 		<div class="col-md-8">
 			<div class="card-body">
-				<h5 class="card-title">Card title</h5>
+				<h5 class="card-title">Hard Drive</h5>
 				<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
 					content. This content is a little bit longer.</p>
-				<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+					<div class="col-5">
+						<p>Specs</p>
+						<ul>
+							<li>
+								<p class="specslist">Faster loading times!</p>
+							</li>
+							<li>
+								<p class="specslist">Get it now!</p>
+							</li>
+						</ul>
+					</div>
+				<p class="card-text"><small class="text-muted">Last updated 53 mins ago</small></p>
 			</div>
 		</div>
 		<div class="col-md-4">
-			<img src="./img/ram.PNG" class="card-img" alt="Ram Stick">
+			<img src="https://drive.google.com/uc?export=view&id=1ND-N-nIYrZo_v5fbzMCOVjr0IKd8Jw3G" class="card-img" alt="Hard Drive">
 		</div>
 	</div>
 </div>
@@ -134,7 +166,18 @@
 				<h5 class="card-title">Card title</h5>
 				<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
 					content. This content is a little bit longer.</p>
-				<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+					<div class="col-5">
+						<p>Specs</p>
+						<ul>
+							<li>
+								<p class="specslist">Faster loading times!</p>
+							</li>
+							<li>
+								<p class="specslist">Get it now!</p>
+							</li>
+						</ul>
+					</div>
+				<p class="card-text"><small class="text-muted">Last updated 10 mins ago</small></p>
 			</div>
 		</div>
 	</div>
